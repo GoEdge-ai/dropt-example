e71823ba4be56baefbdea1d5729ca1ab63320f5d
Haopin Wu <psistwu@outlook.com>
2020-07-20 07:08:11 +0800

    Add five trial examples

---
+++ trials/func-eggholder/variable.py
@@ -0,0 +1,1 @@
+../../util/variable.py
---
+++ util/variable.py
@@ -0,0 +1,9 @@
+'''Important variables'''
+
+
+from pathlib import Path
+
+
+DATA_PREFIX = Path('../../data')
+LOG_DIR = Path('../../log')
+LOG_DIR.mkdir(exist_ok=True)
